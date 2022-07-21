c83a9c39b5483615d54a9f19343868513c8a94d1
yokesh-ks <ksyokesh98@gmail.com>
2022-07-21 23:24:45 +0530

    feat(caurosel added): caurousel Added

---
+++ packages/slider-swipe/components/caurosel.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+function Caurosel() {
+  return <MainContaniner>caurosel</MainContaniner>;
+}
+
+const MainContaniner = styled.div`
+    max-width: 600px;
+    width: '100%';
+    border: 8px solid red;
+    border-radius: 10px;
+`
+
+export default Caurosel;
